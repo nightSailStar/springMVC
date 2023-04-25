a23fe5b0883c87398f591b1e5dfea00453fee258
cheng.bo <2837393034@qq.com>
2023-04-25 17:02:48 +0800

    Revert "基本功能全部开发成功2"
    This reverts commit c0e715b5

--- src/main/java/com/example/springmvc/service/ProjectService.java
+++ src/main/java/com/example/springmvc/service/ProjectService.java
@@ -1,24 +1,10 @@
 package com.example.springmvc.service;
 
 import com.example.springmvc.pojo.Project;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 
 public interface ProjectService {
-
-    List<Project> getAllProject();
-
     Project getProjectById(int project_id);
-
-    List<Project> getProjectByName(String projectName);
-
-    List<Project> getProjectByStatus(String status);
-
-    int insertProject(Project project);
-
-    int updateProject(Project project);
-
-    int deleteProject(int projectId);
 }
--- src/main/java/com/example/springmvc/service/StaffJoinServiceImpl.java
+++ src/main/java/com/example/springmvc/service/StaffJoinServiceImpl.java
@@ -1,9 +1,10 @@
 package com.example.springmvc.service;
 
 import com.example.springmvc.dao.ProjectDao;
-import com.example.springmvc.dao.StaffDao;
 import com.example.springmvc.dao.StaffJoinProjectDao;
-import com.example.springmvc.pojo.*;
+import com.example.springmvc.pojo.Project;
+import com.example.springmvc.pojo.StaffAndProject;
+import com.example.springmvc.pojo.StaffJoinProject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +19,6 @@ public class StaffJoinServiceImpl implements StaffJoinService {
     StaffJoinProjectDao staffJoinProjectDao;
     @Autowired
     ProjectDao projectDao;
-    @Autowired
-    StaffDao staffDao;
 
 //    通过员工信息，返回传递给前端的对象StaffAndProject类
     @Override
@@ -47,36 +46,6 @@ public class StaffJoinServiceImpl implements StaffJoinService {
             staffAndProject.setSupportBeginTime(sjp.getSupportBeginTime());
             staffAndProject.setSupportEndTime(sjp.getSupportEndTime());
             res.add(staffAndProject);
-        }
-        return res;
-    }
-
-
-    @Override
-    public List<ProjectAndStaff> getAllStaffByProjectId(int projectId) {
-        List<StaffJoinProject> list = staffJoinProjectDao.getAllStaffByProjectId(projectId);
-        return getStaffInfo(list);
-    }
-
-    public List<ProjectAndStaff> getAllStaffByTime(int projectId, Date beginTime, Date endTime){
-        List<StaffJoinProject> list = staffJoinProjectDao.getStaffByTime(projectId,beginTime,endTime);
-        return getStaffInfo(list);
-    }
-
-
-    private List<ProjectAndStaff> getStaffInfo(List<StaffJoinProject> list){
-        List<ProjectAndStaff> res = new ArrayList<>();
-        for(StaffJoinProject sjp : list){
-            int staffId = sjp.getStaffId();
-            Staff staff = staffDao.getByStaffId(staffId);
-            ProjectAndStaff projectAndStaff = new ProjectAndStaff();
-            projectAndStaff.setStaffId(staff.getStaffId());
-            projectAndStaff.setStaffName(staff.getStaffName());
-            projectAndStaff.setDepartment(staff.getDepartment());
-            projectAndStaff.setPosition(staff.getPosition());
-            projectAndStaff.setSupportBeginTime(sjp.getSupportBeginTime());
-            projectAndStaff.setSupportEndTime(sjp.getSupportEndTime());
-            res.add(projectAndStaff);
         }
         return res;
     }
